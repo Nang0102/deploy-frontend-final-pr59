e93ae84fe73047669b8dcb155e8e99a647ee8c8d
NgocBaoTa <96873510+NgocBaoTa@users.noreply.github.com>
2022-07-23 10:46:19 +0700

    Update user.js

--- back-end/router/user.js
+++ back-end/router/user.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const bcrypt = require("bcrypt");
 const { db } = require("../db");
 const userRouter = express.Router();
@@ -94,51 +93,4 @@ let checkUserEmail = (userEmail) => {
 };
 
 module.exports = userRouter;
-=======
-const { ObjectId } = require("mongodb");
-const userRouter = express.Router();
-const { db } = require("../db");
-const bcrypt = require('bcrypt')
-const jwt =require('jsonwebtoken')
-const jwtKey = require("./key");
 
-userRouter.post("/sign-up", async (req, res) => {
-    const { username,email, password } = req.body;
-    const saltRounds = 10;
-    await bcrypt.hash(password, saltRounds,async function(err, hash) {
-        
-        const respond = await db.users.insertOne({
-          username,
-          email,
-          password: hash,
-        });
-
-        res.status(201);
-        res.json(respond)
-        if (err) {
-            res.status(500)
-            res.json(err)
-        }
-    });
-    return
-  });
-
-  userRouter.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-    const userFromDB = await db.users.findOne({
-        username
-    })
-    const isPasswordMatch = await bcrypt.compare( password, userFromDB.password)
-
-  if (isPasswordMatch === false) {
-    res.status(401);
-    res.json("User is not existed");
-    return;
-  }
-//
-  const token = jwt.sign(userFromDB, jwtKey);
-  res.json({ token: token });
-});
-
-module.exports = userRouter
->>>>>>> develop
