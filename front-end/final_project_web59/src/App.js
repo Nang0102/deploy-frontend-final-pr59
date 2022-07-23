5d49c9f4b9bcc27d65f6f1063bbd2758c0c532e4
Mai Mai <ntmainbm6328@gmail.com>
2022-07-23 11:06:18 +0700

    resole conflict

--- front-end/final_project_web59/src/App.js
+++ front-end/final_project_web59/src/App.js
@@ -5,18 +5,11 @@ import Login from "./components/auth/login/Login";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Login />} />
       </Routes>
-=======
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
->>>>>>> develop
     </div>
   );
 }
