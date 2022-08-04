02f926b44aa4885e216e051371ba39700fdf2e4f
Mai Mai <ntmainbm6328@gmail.com>
2022-08-04 15:11:57 +0700

    add

--- src/components/rooms/Detail/Detail.js
+++ src/components/rooms/Detail/Detail.js
@@ -1,6 +1,24 @@
 import "../rooms.css";
+import { React, useState, useEffect } from "react";
+import { axios } from "axios";
 
 export const Detail = () => {
+  const url = "http://127.0.0.1:5000/user/room";
+  const [room, setRoom] = useState(null);
+  //
+  let content = null;
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setRoom(response.data);
+    });
+  }, [url]);
+  if (room) {
+    content = (
+      <div>
+        <h1>{room.title}</h1>
+      </div>
+    );
+  }
   return (
     <div>
       <div
@@ -11,6 +29,7 @@ export const Detail = () => {
         }}
       >
         <p className="text-rooms">Rooms Detail</p>
+        {content}
       </div>
     </div>
   );
--- src/components/rooms/Rooms.js
+++ src/components/rooms/Rooms.js
@@ -1,7 +1,8 @@
 import "./rooms.css";
 import { Search } from "./Search/Search";
-import { Style } from "./Styles/Style";
+// import { Style } from "./Styles/Style";
 import roomData from "./rooms.json";
+import { Detail } from "./Detail/Detail";
 const Room = () => {
   return (
     <div>
@@ -18,7 +19,8 @@ const Room = () => {
         <Search placeholder="Search..." data={roomData} />
       </div>
       <div>
-        <Style />
+        {/* <Style /> */}
+        <Detail />
       </div>
     </div>
   );
